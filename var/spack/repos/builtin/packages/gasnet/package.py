81474de51c04499c04af5d9c15e8b761e7c5cb2c
Christoph Junghans <junghans@lanl.gov>
2017-04-22 20:27:33 -0600

    gasnet: build with fPIC for shared lib consumers

--- var/spack/repos/builtin/packages/gasnet/package.py
+++ var/spack/repos/builtin/packages/gasnet/package.py
@@ -40,7 +40,7 @@ class Gasnet(AutotoolsPackage):
     version('1.24.0', 'c8afdf48381e8b5a7340bdb32ca0f41a')
 
     variant('ibv', default=False, description="Support InfiniBand")
-    variant('mpi', default=False, description="Support MPI")
+    variant('mpi', default=True, description="Support MPI")
 
     depends_on('mpi', when='+mpi')
 
@@ -52,7 +52,8 @@ class Gasnet(AutotoolsPackage):
             "--enable-par",
             "--enable-smp",
             "--enable-udp",
-            "--enable-mpi-compat",
+            "--enable-mpi-compat"
+            if '+mpi' in self.spec else '--disable-mpi-compat',
             "--enable-smp-safe",
             "--enable-segment-fast",
             "--disable-aligned-segments",
@@ -60,5 +61,11 @@ class Gasnet(AutotoolsPackage):
             # See the Legion webpage for details on when to/not to use.
             "--disable-pshm",
             "--with-segment-mmap-max=64MB",
+            # for consumers with shared libs
+            "CC=%s %s" % (spack_cc, self.compiler.pic_flag),
+            "CXX=%s %s" % (spack_cxx, self.compiler.pic_flag),
+            "MPI_CC=%s %s" % (
+            ( self.spec['mpi'].mpicc, self.compiler.pic_flag )
+            if '+mpi' in self.spec else ('','') )
         ]
         return args
